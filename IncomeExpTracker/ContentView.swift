ed595fafc374029f578c4516da747be683e46106
Abhishek Bhalerao <abhishekbhalerao.appdev@gmail.com>
2025-06-29 18:18:21 +0530

    Initial Commit

---
+++ IncomeExpTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IncomeExpTracker
+//
+//  Created by Abhishek Bhalerao on 29/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IncomeExpTracker/IncomeExpTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeExpTrackerApp.swift
+//  IncomeExpTracker
+//
+//  Created by Abhishek Bhalerao on 29/06/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeExpTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
